718cca408a6abccddc66d3e5a7333820f80abacf
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-10-06 14:26:04 +0530

    v2

--- script.js
+++ script.js
@@ -14,55 +14,56 @@ function login(){
     z.style.left = "0px";
 }
 
-document.querySelectorAll(".input-container[data-err] .input").forEach(inpE1 => {
-    inpE1.addEventListener("input", ()=> {
-        inpE1.parentElement.removeAttribute('data-err');
-    });
-});
-
 // form validation
 
-let id = (id) => document.getElementById(id);
+function clearErrors(){
 
-let classes = (classes) => document.getElementsByClassName(classes);
-
-let username = id("username"),
-  email = id("email"),
-  password = id("password"),
-  form = id("form"),
-  errorMsg = classes("error"),
-  successIcon = classes("success-icon"),
-  failureIcon = classes("failure-icon");
-
-// Adding the submit event Listener
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  engine(username, 0, "Username cannot be blank");
-  engine(email, 1, "Email cannot be blank");
-  engine(password, 2, "Password cannot be blank");
-});
-
-// engine function which will do all the works
-
-let engine = (id, serial, message) => {
-  if (id.value.trim() === "") {
-    errorMsg[serial].innerHTML = message;
-    id.style.border = "2px solid red";
-
-    // icons
-    failureIcon[serial].style.opacity = "1";
-    successIcon[serial].style.opacity = "0";
-  } else {
-    errorMsg[serial].innerHTML = "";
-    id.style.border = "2px solid green";
-
-    // icons
-    failureIcon[serial].style.opacity = "0";
-    successIcon[serial].style.opacity = "1";
-  }
-};
+    errors = document.getElementsByClassName('formerror');
+    for(let item of errors)
+    {
+        item.innerHTML = "";
+    }
 
 
+}
+function seterror(id, error){
+    //sets error inside tag of id 
+    element = document.getElementById(id);
+    element.getElementsByClassName('formerror')[0].innerHTML = error;
 
+}
+
+function validateForm(){
+    var returnval = true;
+    clearErrors();
+    var name = document.forms['myForm']["fname"].value;
+    if (name.length<5){
+        seterror("name", "*Length of name is too short");
+        returnval = false;
+    }
+
+    if (name.length == 0){
+        seterror("name", "*Length of name cannot be zero!");
+        returnval = false;
+    }
+
+    var email = document.forms['myForm']["femail"].value;
+    if (email.length>20){
+        seterror("email", "*Email length is too long");
+        returnval = false;
+    }
+
+    var password = document.forms['myForm']["fpass"].value;
+    if (password.length < 6){
+        seterror("pass", "*Password should be atleast 6 characters long!");
+        returnval = false;
+    }
+
+    var cpassword = document.forms['myForm']["fcpass"].value;
+    if (cpassword != password){
+        seterror("cpass", "*Password and Confirm password should match!");
+        returnval = false;
+    }
+
+    return returnval;
+}
