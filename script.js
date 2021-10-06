f12182d5a4c8013936fed2ee0a7e66a886fb81d6
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-10-06 12:30:17 +0530

    v1

---
+++ script.js
@@ -0,0 +1,68 @@
+let x = document.getElementById("login");
+let y = document.getElementById("sign-up");
+let z = document.getElementById("btn");
+
+function signUp(){
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+
+function login(){
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0px";
+}
+
+document.querySelectorAll(".input-container[data-err] .input").forEach(inpE1 => {
+    inpE1.addEventListener("input", ()=> {
+        inpE1.parentElement.removeAttribute('data-err');
+    });
+});
+
+// form validation
+
+let id = (id) => document.getElementById(id);
+
+let classes = (classes) => document.getElementsByClassName(classes);
+
+let username = id("username"),
+  email = id("email"),
+  password = id("password"),
+  form = id("form"),
+  errorMsg = classes("error"),
+  successIcon = classes("success-icon"),
+  failureIcon = classes("failure-icon");
+
+// Adding the submit event Listener
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  engine(username, 0, "Username cannot be blank");
+  engine(email, 1, "Email cannot be blank");
+  engine(password, 2, "Password cannot be blank");
+});
+
+// engine function which will do all the works
+
+let engine = (id, serial, message) => {
+  if (id.value.trim() === "") {
+    errorMsg[serial].innerHTML = message;
+    id.style.border = "2px solid red";
+
+    // icons
+    failureIcon[serial].style.opacity = "1";
+    successIcon[serial].style.opacity = "0";
+  } else {
+    errorMsg[serial].innerHTML = "";
+    id.style.border = "2px solid green";
+
+    // icons
+    failureIcon[serial].style.opacity = "0";
+    successIcon[serial].style.opacity = "1";
+  }
+};
+
+
+
